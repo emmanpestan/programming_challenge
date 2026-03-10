32bba2ea655d749bf1b755338a5f0a3c7e7fc85e
unknown <emmanuel.empestan@gmail.com>
2026-03-11 01:05:47 +0800

    batch02prog01

---
+++ 2_numbers_print_smaller_number.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if num1 > num2:
+    print(f"{num1} is greater than {num2}")
+else:
+    print(f"{num2} is greater than {num1}")
