d355bb3351630ef61ffe3dc2e2eca183b009ec45
unknown <emmanuel.empestan@gmail.com>
2026-03-13 03:32:31 +0800

    input numbers prints lowest

--- 10_numbers_print_even_numbers.py
+++ 10_numbers_print_even_numbers.py
@@ -1,8 +1,5 @@
-count = 0
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
-for i in range(10):
-    num = int(input("Enter number: "))
-    if num % 2 == 0:
-        count += 1
-
-print("The even count is:", count)
+for i in range(num1 + 1, num2):
+    print(i)
