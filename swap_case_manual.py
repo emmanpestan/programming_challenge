0fbe86ea55bc7a339805e8bb0274d3ebc882a27d
unknown <emmanuel.empestan@gmail.com>
2026-03-20 21:18:22 +0800

    swap cases manually

---
+++ swap_case_manual.py
@@ -0,0 +1,12 @@
+text = input()
+result = ""
+
+for ch in text:
+    if 'a' <= ch <= 'z':
+        result += chr(ord(ch) - 32)
+    elif 'A' <= ch <= 'Z':
+        result += chr(ord(ch) + 32)
+    else:
+        result += ch
+
+print(result)
